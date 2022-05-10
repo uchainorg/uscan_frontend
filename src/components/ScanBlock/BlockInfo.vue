fe219f06d025175b1aad165ce7cc25ec4bbff99e
levtea <rzh2000@163.com>
2022-05-10 16:04:27 +0800

    fix

--- src/components/ScanBlock/BlockInfo.vue
+++ src/components/ScanBlock/BlockInfo.vue
@@ -2,7 +2,7 @@
   <div class="container" style="margin-top: 2%; max-width: 1350px">
     <h3 style="display: inline">Block</h3>
     <p style="display: inline; margin-left: 1%">#{{ number }}</p>
-    <el-tabs v-model="activeName" @tab-click="handleClick" style="">
+    <el-tabs v-model="activeName" style="">
       <el-tab-pane label="Overview" name="first">
         <block-overview :data="blockOverviewData"></block-overview>
       </el-tab-pane>
@@ -23,11 +23,11 @@ export default {
       blockCommentsData: { blockNumber: this.number },
     };
   },
-  methods: {
-    handleClick(tab) {
-      console.log(tab);
-    },
-  },
+  // methods: {
+  //   handleClick(tab) {
+  //     console.log(tab);
+  //   },
+  // },
 };
 </script>
 <style lang=""></style>
