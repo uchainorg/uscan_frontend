5844c55cfb2f8eefea9acb737eba8e4f83455de1
levtea <rzh2000@163.com>
2022-05-27 16:47:26 +0800

    tail

--- src/components/ScanTail/ScanTail.vue
+++ src/components/ScanTail/ScanTail.vue
@@ -1,5 +1,14 @@
 <template lang="">
-  <div class="tail-container">Let's do something interesting.End!</div>
+  <!-- <div class="tail-container">Let's do something interesting.End!</div> -->
+  <div style="background-color: white; margin-top: 3%">
+    <div style="width: 1350px; height: 140px; margin: 0 auto; display: flex; justify-content: center; align-items: center">
+      <div class="center-row">
+        <el-icon color="#253258" :size="33"><Bowl /></el-icon>
+        &nbsp;
+        <h2>Let's do something interesting.End!</h2>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
@@ -8,9 +17,5 @@ export default defineComponent({
 });
 </script>
 <style lang="less" scoped>
-.tail-container {
-  display: flex;
-  margin-top: 50px;
-  justify-content: center;
-}
+@import "../../css/style.css";
 </style>
