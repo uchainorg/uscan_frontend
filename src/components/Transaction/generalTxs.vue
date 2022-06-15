9819c4363df037951dc66c77ba701b2f3d895509
levtea <rzh2000@163.com>
2022-06-15 12:11:36 +0800

    fix

--- src/components/Transaction/generalTxs.vue
+++ src/components/Transaction/generalTxs.vue
@@ -1,9 +1,10 @@
 <template lang="">
   <div>
-    <el-table :data="txsData" empty-text="loading..." style="width: 100%; border-radius: 0.35rem" :row-style="{ height: '50px' }">
+    <el-table :data="txsData" style="width: 100%; border-radius: 0.35rem" :row-style="{ height: '50px' }">
+      <template #empty>{{ emptyText }}</template>
       <el-table-column width="37px">
         <template v-slot:default="scope">
-          <el-popover placement="bottom" title="Additional Info" :width="320" trigger="click">
+          <el-popover placement="right" title="Additional Info" :width="320" trigger="click">
             <template #reference>
               <el-button style="width: 5px" type="info" size="small">
                 <el-icon><View /></el-icon>
@@ -86,11 +87,16 @@ export default defineComponent({
       require: true,
     },
   },
+  data() {
+    return {
+      emptyText: "loading",
+    };
+  },
   watch: {
     txsData(newVal) {
       // console.log(newVal.length);
       if (newVal.length == 0) {
-        this.$router.push("/notFound");
+        this.emptyText = "No data found";
       }
     },
   },
