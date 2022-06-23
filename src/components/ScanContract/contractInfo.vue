4b176a298bb350b71614e63683e828b7b57a0f12
levtea <rzh2000@163.com>
2022-06-23 18:31:58 +0800

    fix

--- src/components/ScanContract/contractInfo.vue
+++ src/components/ScanContract/contractInfo.vue
@@ -1,16 +1,11 @@
 <template lang="">
   <div>
     <div class="center-row" style="font-size: 14px">
-      <el-icon size="large"><InfoFilled /></el-icon> &nbsp; (test)Are you the contract creator?&nbsp;
+      <el-icon size="large"><InfoFilled /></el-icon> &nbsp; Are you the contract creator?&nbsp;
       <router-link :to="'/verifyContract?a=' + contractAddress" style="font-size: 15px; font-weight: bold"> Verify and Publish </router-link>
       &nbsp;your contract source code today!
     </div>
     <br />
-    <!-- <div style="font-size: 14px">
-      (test)Note: We also found another&nbsp;
-      <router-link :to="'/'" style="font-size: 15px; font-weight: bold"> 1 </router-link>
-      &nbsp;contract with exact matching byte codes
-    </div> -->
 
     <textarea class="byte-codes-text" rows="15" v-model="codeContent"> </textarea>
   </div>
