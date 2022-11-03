5a74e6e6cba2f06d91c2fadc8a335d7e233ceaee
levtea <rzh2000@163.com>
2022-11-03 13:28:30 +0800

    originUrl

--- src/script/service/request.ts
+++ src/script/service/request.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { ResponseType } from '../model/index';
 
+const originUrl = window.location.origin + '/chain-scan';
+
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL == '' ? originUrl : import.meta.env.VITE_BASE_URL,
 });
 
 const request = async <T = any>(config: AxiosRequestConfig): Promise<ResponseType<T>> => {
