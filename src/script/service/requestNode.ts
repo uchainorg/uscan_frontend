98a5d9cca04826c2b89cfc6c8e465b044692f6b3
levtea <rzh2000@163.com>
2022-11-29 19:41:35 +0800

    VITE_NODE_URL path

--- src/script/service/requestNode.ts
+++ src/script/service/requestNode.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const requestNode = axios.create({
-  baseURL: import.meta.env.VITE_NODE_URL,
+  baseURL: import.meta.env.VITE_NODE_URL == undefined ? 'https://testnet.ankr.com' : import.meta.env.VITE_NODE_URL,
 });
 
 export default requestNode;
